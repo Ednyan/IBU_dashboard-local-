39b6029153dc835d018c79118b583c35032b65e0
KoolKid6958 <156125488+KoolKid6958@users.noreply.github.com>
2025-10-04 22:56:50 -0400

    feat(setup): add rust build step

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+# Python setup
 echo "Creating venv"
 python3 -m venv .venv
 echo "Installing deps"
 ./.venv/bin/python -m pip install -r requirements.txt
+
+# Rust setup
+./scripts/build.sh
 
 echo "To run, run ./scripts/run.sh"
