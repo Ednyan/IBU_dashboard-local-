147b8289f94a4125728b75f0cdd157ab0dcceca8
KoolKid6958 <156125488+KoolKid6958@users.noreply.github.com>
2025-12-08 20:46:10 -0500

    chore: rename IBU_dashboard.py to main.py

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 crond
-./.venv/bin/uwsgi --http 0.0.0.0:5000 --master --enable-threads --lazy-apps -w IBU_dashboard:app
+./.venv/bin/uwsgi --http 0.0.0.0:5000 --master --enable-threads --lazy-apps -w main:app
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./.venv/bin/uwsgi --http 0.0.0.0:5000 --master --enable-threads --lazy-apps -w IBU_dashboard:app
+./.venv/bin/uwsgi --http 0.0.0.0:5000 --master --enable-threads --lazy-apps -w main:app
